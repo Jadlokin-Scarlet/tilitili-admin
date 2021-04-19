b17140a613e8f15323d05b1d7b404543ea5badd0
黄克凡 <w545459363@126.com>
2021-04-19 20:41:21 +0800

    commit

--- src/pages/recommend/RecommendTalkManager/index.jsx
+++ src/pages/recommend/RecommendTalkManager/index.jsx
@@ -7,7 +7,7 @@ import BatchUpdateRecommendTalk from "./BatchUpdateRecommendTalk";
 export default class RecommendTalkManager extends Component {
     columnsConfig = [
         {title: '模块', key: 'type', width: 70, type: 'choose', chooseMap: 'typeList'},
-        {title: '主持人', key: 'speaker', width: 70, type: 'chooseRender', chooseMap: 'speakerList'},
+        {title: '主持人', key: 'speaker', width: 70, type: 'choose', chooseMap: 'speakerList'},
         {title: '台本', key: 'text', width: 500},
         {title: '表情', key: 'expression', width: 70},
         {title: '所在刊', key: 'issueId', width: 150, ellipsis: true, type: 'chooseInput', chooseMap: 'recommendIssueResource'},
@@ -17,7 +17,7 @@ export default class RecommendTalkManager extends Component {
     handleResourcesInit = () => ({
         typeList: [{text: 'OP', value: 1}, {text: 'ED', value: 3}],
         statusList: [{text: '正常', value: 0, renderHidden: true}, {text: '废弃', value: -1}],
-        speakerList: [{text: '灵梦', value: '灵梦', tag: 'red'}, {text: '早苗', value: '早苗', tag: 'green'}],
+        speakerList: [{text: '灵梦', value: '灵梦', tag: 'red'}, {text: '早苗', value: '早苗', tag: 'green'}, {text: '灵梦&早苗', value: '灵梦&早苗', tag: 'no'}],
     })
 
     constructor(props) {
