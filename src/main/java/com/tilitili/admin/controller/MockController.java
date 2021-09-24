0375e7ba4eb101a5e1b59dc2f9a303523e666f3d
huangkf <huangkf_sx@taofen8.com>
2021-09-24 17:34:39 +0800

    debug

--- src/main/java/com/tilitili/admin/controller/MockController.java
+++ src/main/java/com/tilitili/admin/controller/MockController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.HttpCookie;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/api/mock")
@@ -25,6 +29,7 @@ public class MockController {
         String requestURL = request.getRequestURL().toString();
         String queryString = request.getQueryString();
         body = body == null? "": body;
-        miraiManager.sendFriendMessage("Plain", requestURL + "?" + queryString + "\n" + body);
+        String cookie = Arrays.stream(request.getCookies()).map(c -> String.format("%s=%s", c.getName(), c.getValue())).collect(Collectors.joining("; "));
+        miraiManager.sendFriendMessage("Plain", String.format("%s?%s\nbody=%s\ncookie=%s", requestURL, queryString, body, cookie));
     }
 }
