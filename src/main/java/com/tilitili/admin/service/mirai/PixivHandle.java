952b5b29d380a7101700ac084931b17de8e00386
huangkf <huangkf_sx@taofen8.com>
2021-10-08 18:41:22 +0800

    色图2.0！

--- src/main/java/com/tilitili/admin/service/mirai/PixivHandle.java
+++ src/main/java/com/tilitili/admin/service/mirai/PixivHandle.java
@@ -63,7 +63,7 @@ public class PixivHandle implements BaseMessageHandle {
             String imageUrl = dataList.get(index).getUrl();
             String id = dataList.get(index).getId();
             String subUrl = StringUtil.matcherGroupOne("(/img/..../../../../../../)", imageUrl);
-            String type = StringUtil.matcherGroupOne("((!png|jpg))", imageUrl);
+            String type = StringUtil.matcherGroupOne("((?:png|jpg))", imageUrl);
             if (subUrl == null || type == null) {
                 miraiManager.sendFriendMessage("Plain", "异常 id = " + id + " url=" + imageUrl);
                 lockFlag.set(false);
