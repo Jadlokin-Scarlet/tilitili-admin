f76c25e847cf81aabed3cd3fc0d695ed61a07a05
huangkf <huangkf_sx@taofen8.com>
2021-10-09 18:17:20 +0800

    色图2.0！

--- src/main/java/com/tilitili/admin/service/mirai/PixivHandle.java
+++ src/main/java/com/tilitili/admin/service/mirai/PixivHandle.java
@@ -77,7 +77,7 @@ public class PixivHandle implements BaseMessageHandle {
                 bigImageList = pixivManager.getBigImageList(noUsedImage.getPid());
                 Asserts.isFalse(bigImageList.isEmpty(), "读不到大图");
                 pixivImageMapper.updatePixivImage(new PixivImage().setId(noUsedImage.getId()).setUrlList(String.join(",", bigImageList)));
-                TimeUnit.MILLISECONDS.sleep(100);
+                TimeUnit.MILLISECONDS.sleep(200);
             } else {
                 bigImageList = Arrays.stream(noUsedImage.getUrlList().split(",")).collect(Collectors.toList());
             }
@@ -85,6 +85,7 @@ public class PixivHandle implements BaseMessageHandle {
             List<String> imageIdList = bigImageList.stream().map(StreamUtil.tryMap(imageUrl -> {
                 String type = StringUtil.matcherGroupOne("((?:png|jpg))", imageUrl);
                 BufferedImage image = pixivManager.downloadImage(imageUrl);
+                TimeUnit.MILLISECONDS.sleep(200);
                 File tempFile = File.createTempFile("pixivImage", "."+type);
                 ImageIO.write(image, type, tempFile);
                 String imageId = miraiManager.uploadImage(tempFile);
