0a8bf160e5a76c9c8ec7cfa233a008d51e417a46
huangkf <huangkf_sx@taofen8.com>
2021-10-18 14:25:28 +0800

    色图4.0!

--- src/main/java/com/tilitili/admin/service/mirai/PixivHandle.java
+++ src/main/java/com/tilitili/admin/service/mirai/PixivHandle.java
@@ -162,7 +162,7 @@ public class PixivHandle implements BaseMessageHandle {
 //            imageIdList.add(imageId);
 //            imageIdList.add(imageUrl.replace("https://", "https://api.pixiv.moe/image/"));
 //        }
-        Integer messageId = miraiManager.sendMessage(new MiraiMessage().setMessageType("ImageText").setSendType("group").setUrl(url.replace("https://", "https://api.pixiv.moe/image/")).setMessage("https://pixiv.moe/illust/"+pid).setGroup(sendGroup.getId()));
+        Integer messageId = miraiManager.sendMessage(new MiraiMessage().setMessageType("ImageText").setSendType("group").setUrl(url.replace("https://", "https://api.pixiv.moe/image/")).setMessage("https://pixiv.moe/illust/"+pid+"\n").setGroup(sendGroup.getId()));
         pixivImageMapper.updatePixivImage(new PixivImage().setId(noUsedImage.getId()).setStatus(1));
         return messageId;
     }
