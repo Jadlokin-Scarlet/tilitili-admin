cb36a85ce91cc446067a7c2e39a86702b6ab685c
黄克凡 <w545459363@126.com>
2021-11-04 11:11:43 +0800

    mapper move

--- src/main/java/com/tilitili/StartApplication.java
+++ src/main/java/com/tilitili/StartApplication.java
@@ -1,8 +1,11 @@
 package com.tilitili;
 
+import org.apache.ibatis.io.VFS;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,10 +13,11 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @EnableRetry
 @EnableCaching
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StartApplication {
 
     public static void main(String[] args) {
+        VFS.addImplClass(SpringBootVFS.class);
         SpringApplication.run(StartApplication.class, args);
     }
 }
