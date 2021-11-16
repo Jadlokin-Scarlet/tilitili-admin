d601ab3248446405039ae04e271c57b70e5ab7d7
黄克凡 <w545459363@126.com>
2021-11-16 14:30:38 +0800

    find image

--- src/main/java/com/tilitili/admin/service/mirai/FindImageHandle.java
+++ src/main/java/com/tilitili/admin/service/mirai/FindImageHandle.java
@@ -3,8 +3,8 @@ package com.tilitili.admin.service.mirai;
 import com.google.common.collect.ImmutableMap;
 import com.tilitili.admin.emnus.MessageHandleEnum;
 import com.tilitili.admin.entity.mirai.MiraiRequest;
+import com.tilitili.common.entity.mirai.MessageChain;
 import com.tilitili.common.entity.mirai.MiraiMessage;
-import com.tilitili.common.entity.mirai.MiraiMessageView;
 import com.tilitili.common.utils.Asserts;
 import com.tilitili.common.utils.HttpClientUtil;
 import org.jsoup.Jsoup;
@@ -13,9 +13,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Component
 public class FindImageHandle implements BaseMessageHandle{
@@ -36,9 +35,18 @@ public class FindImageHandle implements BaseMessageHandle{
         Elements imageList = document.select(".result:not(.hidden):not(#result-hidden-notification)");
         Asserts.isFalse(imageList.isEmpty(), "没找到🤕\n"+url);
         Element image = imageList.get(0);
+
+        List<MessageChain> messageChainArrayList = new ArrayList<>();
+
         String rate = image.select(".resultsimilarityinfo").text();
+        String imageUrl = image.select(".resulttableimage img").attr("src");
         Elements linkList = image.select(".resultcontentcolumn a.linkify");
+        Asserts.notBlank(rate, "没找到😑\n"+url);
+        Asserts.notBlank(imageUrl, "没找到😑\n"+url);
+        Asserts.isFalse(linkList.isEmpty(), "没找到😑\n"+url);
+
         String link = linkList.get(0).attr("href");
-        return result.setMessage(String.format("找到啦😊！相似度%s\n%s", rate, link)).setMessageType("Plain");
+
+        return result.setMessage(String.format("找到啦😊！相似度%s\n%s", rate, link)).setUrl(imageUrl).setMessageType("ImageText");
     }
 }
