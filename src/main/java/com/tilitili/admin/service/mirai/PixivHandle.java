891a8dde53708903c4819af0de99dc0274d3a64a
黄克凡 <w545459363@126.com>
2021-11-23 15:52:23 +0800

    pixiv

--- src/main/java/com/tilitili/admin/service/mirai/PixivHandle.java
+++ src/main/java/com/tilitili/admin/service/mirai/PixivHandle.java
@@ -79,8 +79,8 @@ public class PixivHandle implements BaseMessageHandle {
 
             Integer messageId;
             switch (source) {
-                case "pixiv.moe": messageId = sendPixivMoeImage(sendGroup, searchKey, source); break;
                 case "pixiv": messageId = pixivManager.sendPixivImage(sendMessageId, searchKey, source, r18); break;
+                case "pixiv.moe": messageId = sendPixivMoeImage(sendGroup, searchKey, source); break;
                 case "lolicon": messageId = sendLoliconImage(sendGroup, searchKey, source, num); break;
                 default: throw new AssertException("不支持的平台");
             }
