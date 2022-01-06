5d19b45645f5fa74753750edf1a9a8554f1db382
黄克凡 <w545459363@126.com>
2022-01-06 15:22:13 +0800

    gitlab

---
+++ src/main/java/com/tilitili/admin/controller/GitlabController.java
@@ -0,0 +1,29 @@
+package com.tilitili.admin.controller;
+
+import com.tilitili.common.entity.view.gitlab.GitlabContainer;
+import com.tilitili.common.manager.GitlabManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/gitlab")
+public class GitlabController {
+	private final GitlabManager gitlabManager;
+
+	@Autowired
+	public GitlabController(GitlabManager gitlabManager) {
+		this.gitlabManager = gitlabManager;
+	}
+
+	@RequestMapping("/deleteContainer")
+	public void deleteContainer(String projectName) {
+		Long projectId = gitlabManager.getProjectIdByProjectName(projectName);
+		List<GitlabContainer> containerList = gitlabManager.getContainerList(projectId);
+		for (GitlabContainer gitlabContainer : containerList) {
+			gitlabManager.deleteContainer(gitlabContainer);
+		}
+	}
+}
