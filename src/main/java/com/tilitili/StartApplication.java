0b6a3669abc43e86a2470915dbfff066a4d693f9
黄克凡 <w545459363@126.com>
2022-03-11 16:29:47 +0800

    nacos

--- src/main/java/com/tilitili/StartApplication.java
+++ src/main/java/com/tilitili/StartApplication.java
@@ -6,13 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @EnableRetry
 @EnableCaching
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StartApplication {
 
